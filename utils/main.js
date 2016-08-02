031c51bddec50ab69d25d97c06a1623f6e68b1f1
alfredlau <jian.liu@icloud.com>
2016-08-02 20:03:34 +0800

    添加了多页面抓取；添加了一些详细信息

--- utils/main.js
+++ utils/main.js
@@ -3,9 +3,7 @@ const request = require('request');
 const url = require('url');
 const util = require('util');
 
-
 const webPrefix = 'http://bj.lianjia.com/zufang/';
-
 
 // 1.抓取首页，判断多少套房，进而判断多少页，作为循环参数
 
@@ -13,8 +11,6 @@ var destId = [];
 
 function getContents(id) {
     var dstUrl = url.resolve(webPrefix, id) + ".html";
-
-    console.log(dstUrl);
     request(dstUrl, (err, res, body) => {
         if (!err && res.statusCode == 200) {
             var $ = cio.load(body);
@@ -23,7 +19,7 @@ function getContents(id) {
             // 所在区域
             var region = $('div.zf-room p').eq(6).find('a').text();
             // 租金
-            var rent = ' ';
+            var rent = $('div.content.zf-content span.total').text();
             // 面积
             var area = $('div.zf-room p').eq(0).text();
             // 房型
@@ -35,19 +31,21 @@ function getContents(id) {
             // 地铁情况
             var subway = $('div.zf-room p').eq(4).text();
             // 房源特色
-            var special = '';
+            var special = $('div.featureContent span').text();
             // 其他（更新时间，多少人看过此房，同小区成交记录，特殊标示（自如））
             var others = '';
             var source = {
                 id: _id,
                 region: region,
+                rent: rent,
                 area: area,
                 type: type,
                 floor: floor,
                 dir: dir,
-                subway: subway
+                subway: subway,
+                special: special,
+                others: others
             };
-
             console.log(util.inspect(source));
         } else {
             console.log(err);
@@ -55,17 +53,44 @@ function getContents(id) {
     });
 }
 
+function cb(listUrl) {
+    request(listUrl, (err, res, body) => {
+        if (!err && res.statusCode == 200) {
+            $ = cio.load(body);
+            $("ul#house-lst li").each((index, elem) => {
+                //和jquery转换为dom的语法完全一致
+                var id = $(elem).attr('data-id');
+                return getContents(id);
+            });
+        } else {
+            console.log(err);
+        }
+    });
+}
+
+function getUrlAsyn(listUrl) {
+    return cb(listUrl);
+}
+
 request.get(webPrefix, (err, res, body) => {
     if (!err && res.statusCode == 200) {
         $ = cio.load(body);
-        $("ul#house-lst li").each((index, elem) => {
-            //和jquery转换为dom的语法完全一致
-            var id = $(elem).attr('data-id');
-            getContents(id);
+        // 判断多少页
+        var pageNumString = $('div.page-box.house-lst-page-box').attr('page-data');
+        var pageNum = parseInt(JSON.parse(pageNumString).totalPage, 10);
 
-            // destId.push();
-        });
-
+        for (var i = 1; i < pageNum + 1; i++) {
+            if (i == 1) {
+                $("ul#house-lst li").each((index, elem) => {
+                    //和jquery转换为dom的语法完全一致
+                    var id = $(elem).attr('data-id');
+                    getContents(id);
+                });
+            } else {
+                var listUrl = webPrefix + 'pg' + i.toString() + '/';
+                getUrlAsyn(listUrl);
+            }
+        }
     } else {
         return;
     }
