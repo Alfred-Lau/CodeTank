d2cbc75b329faed8dca5f7f8af706a2e063e89a1
alfredlau <jian.liu@icloud.com>
2016-08-09 19:27:30 +0800

    branch crawler

--- app/schema/taskSchema.js
+++ app/schema/taskSchema.js
@@ -1,9 +1,9 @@
 "use strict";
 import mongoose from 'mongoose'
 
-import option from '../../config/config'
+import options from '../../config/config'
 
-const db = mongoose.createConnection(option.host, option.db);
+const db = mongoose.createConnection(options.mongodb.host, options.mongodb.db);
 
 db.on('error', console.error.bind(console, '连接错误:'));
 db.once('open', function() {
