e4a3b3bbd2488a11d1cd4115689d77f518a643d8
alfredlau <jian.liu@icloud.com>
2016-08-10 00:02:20 +0800

    add echart;api接口;ajax

--- app/controllers/chartanalysis.js
+++ app/controllers/chartanalysis.js
@@ -1,3 +1,23 @@
+import util from 'util';
+import mysql from 'mysql';
+import option from '../../config/config';
+
+let pool = mysql.createPool(option.mysql);
+
 exports.index = function(req, res, next) {
     res.render('chart-index', { title: '萍聚' });
+}
+
+exports.data = function(req, res, next) {
+    pool.getConnection((err, connection) => {
+        if (err) {
+            throw err;
+        }
+        connection.query('select region, rent from housedata limit 10', (err, results) => {
+            if (err)
+                throw err;
+            res.json(JSON.stringify(results));
+        })
+        connection.release();
+    })
 }
--- public/javascripts/chart-index.js
+++ public/javascripts/chart-index.js
@@ -1,23 +1,51 @@
-// 基于准备好的dom，初始化echarts实例
-var myChart = echarts.init(document.getElementById('main'));
+$(() => {
 
-//Ajax获取数据，填充至options
-// 指定图表的配置项和数据
-var option = {
-    tooltip: {},
-    legend: {
-        data: ['销量']
-    },
-    xAxis: {
-        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
-    },
-    yAxis: {},
-    series: [{
-        name: '销量',
-        type: 'bar',
-        data: [5, 20, 36, 10, 10, 20]
-    }]
-};
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('main'));
+    let xdata = [];
+    let value = [];
+    let params = {
 
-// 使用刚指定的配置项和数据显示图表。
-myChart.setOption(option);
+    }
+
+    $.ajax({
+        data: params,
+        url: 'http://localhost:3000/data',
+        type: 'get',
+        dataType: 'json',
+        cache: false,
+        timeout: 5000,
+        success: function(data) {
+            var data = $.parseJSON(data);
+
+            for (let tmp of data) {
+                xdata.push(tmp.region);
+                value.push(tmp.rent);
+            }
+            //Ajax获取数据，填充至options
+            // 指定图表的配置项和数据
+            var option = {
+                tooltip: {},
+                legend: {
+                    data: ['销量']
+                },
+                xAxis: {
+                    data: xdata
+                },
+                yAxis: {},
+                series: [{
+                    name: '销量',
+                    type: 'bar',
+                    data: value,
+                }]
+            };
+
+            // 使用刚指定的配置项和数据显示图表。
+            myChart.setOption(option);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            alert('error ' + textStatus + " " + errorThrown);
+        },
+    })
+
+})
